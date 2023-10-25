e9142904df76ab206e20662cfb28efa4acca892d
lukeskywakeri <79768700+lukeskywakeri@users.noreply.github.com>
2023-10-25 19:05:11 -0400

    Update lab6 (Autumn Nix encode).py
    Everything Works!!!

--- lab6 (Autumn Nix encode).py
+++ lab6 (Autumn Nix encode).py
@@ -1,4 +1,5 @@
 # Autumn Nix's encode file
+
 def encode(password):
     split_password = []
     split_password.extend(password)
@@ -17,24 +18,33 @@ def encode(password):
         element = str(element)
         new_password += element
     return new_password
-
-# print menu options
-print("Menu")
-print("-------------")
-print("1. Encode")
-print("2. Decode")
-print("3. Quit")
-print()
-# prompt for menu input
-option = int(input("Please enter an option: "))
-if option == 1:
-    # ask for password to encode
-    password = str(input("Please enter your password to encode: "))
-    encode(password)
-    print("Your password has been encoded and stored!")
-elif option == 2:
-    # decode encoded password
-    pass
-elif option == 3:
-    # quit
-    pass
+def password_decoder(encoded_password):#Carlos Benavides
+    decoded_password = ''
+    for element in encoded_password:
+        element = int(element)
+        if element < 3:
+            element += 7
+        else:
+            element -= 3
+        element = str(element)
+        decoded_password += element
+    return decoded_password
+main = True
+while main == True: #Carlos
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    print()
+    option = int(input("Please enter an option: "))
+    password = ''
+    if option == 1:
+        password = str(input("Please enter your password to encode: "))
+        encoded_password = encode(password)
+        print("Your password has been encoded and stored!")
+    elif option == 2:#Carlos Benavides
+        new_decoded = password_decoder(encoded_password)
+        print(f"The encoded password is {encoded_password}, and the original password is {new_decoded}.")
+    elif option == 3:
+        main = False
